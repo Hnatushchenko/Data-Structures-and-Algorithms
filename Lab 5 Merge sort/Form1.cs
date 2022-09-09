4b1ebe9c77aae2168673755a467516f312bc7806
Hnatushchenko <gnatushenko.andrij@gmail.com>
2022-09-09 16:14:52 +0300

    Add lab 5 Merge sort

---
+++ Lab 5 Merge sort/Form1.cs
@@ -0,0 +1,85 @@
+﻿namespace Lab_5
+{
+    public partial class Form1 : Form
+    {
+        int _maxValue = 2000;
+        private double[] numbers;
+        public Form1()
+        {
+            numbers = Array.Empty<double>();
+            InitializeComponent();
+        }
+
+        /*
+            * Задано одномірний масив дійсних чисел.
+            * Виключити з нього моду (елемент, який повторюється найчастіше).
+        */
+        double[] ApplyIndividualFunction(double[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return Array.Empty<double>();
+            
+            double mod = 0;
+            int maxNumberOfOccurences = 0;
+            int count = 0;
+            foreach (double number in arr)
+            {
+                count = arr.Count(d => d == number);
+                if (count > maxNumberOfOccurences)
+                {
+                    maxNumberOfOccurences = count;
+                    mod = number;
+                }
+            }
+            return arr.Where(d => d != mod).ToArray();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MergeSort.Sort(numbers);
+            foreach (var item in numbers)
+            {
+                richTextBox3.AppendText(item.ToString() + " ");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numbers.Length == 0)
+                return;
+
+            richTextBox2.AppendText("\nArray after applying individual function:\n");
+            numbers = ApplyIndividualFunction(numbers);
+            foreach (var item in numbers)
+            {
+                richTextBox2.AppendText(item.ToString() + " ");
+            }
+            richTextBox2.AppendText("\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            if (int.TryParse(textBox1.Text, out int numberOfElements))
+            {
+                Random rand = new Random();
+                numbers = new double[numberOfElements];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = rand.Next(_maxValue);
+                }
+
+                richTextBox2.AppendText("Initial array:\n");
+                foreach (var item in numbers)
+                {
+                    richTextBox2.AppendText(item.ToString() + " ");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cannot convert to integer");
+            }
+        }
+    }
+}
---
+++ Lab 5 Merge sort/MergeSort.cs
@@ -0,0 +1,44 @@
+﻿namespace Lab_5
+{
+    public static class MergeSort
+    {
+        public static void Sort(double[] arr)
+        {
+            Sort(arr, 0, arr.Length - 1);
+        }
+
+        public static void Sort(double[] array, int leftIndex, int rightIndex)
+        {
+            if (leftIndex >= rightIndex)
+                return;
+
+            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+            Sort(array, leftIndex, middleIndex);
+            Sort(array, middleIndex + 1, rightIndex);
+
+            double[] leftArray = new double[middleIndex - leftIndex + 1];
+            double[] rightArray = new double[rightIndex - middleIndex];
+
+            for (int i = 0; i < leftArray.Length; i++)
+                leftArray[i] = array[leftIndex + i];
+
+            for (int i = 0; i < rightArray.Length; i++)
+                rightArray[i] = array[middleIndex + i + 1];
+
+            for (int i = 0, j = 0, k = leftIndex; k <= rightIndex; k++)
+            {
+                if ((i < leftArray.Length) && (j >= rightArray.Length || leftArray[i] < rightArray[j]))
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
