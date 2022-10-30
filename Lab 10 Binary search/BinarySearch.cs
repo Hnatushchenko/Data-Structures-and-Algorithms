374d65ca24e1fd37b5b8f5c94abafa4123ee9ef2
Hnatushchenko <gnatushenko.andrij@gmail.com>
2022-10-30 15:53:50 +0200

    Add lab 10 Binary Search

---
+++ Lab 10 Binary search/BinarySearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    public class BinarySearch
+    {
+        public static (int position, int numberOfComparisons) Search(int[] array, int value)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return (0, 0);
+            }
+            int numberOfComparisons = 0;
+            return (Search(array, value, 0, array.Length - 1, ref numberOfComparisons), numberOfComparisons);   
+        }
+        private static int Search(int[] array, int value, int low, int high, ref int numberOfComparisons)
+        {
+            numberOfComparisons++;
+            int middle = low + (high - low) / 2;
+            if (low > high)
+            {
+                throw new ArgumentException("Value was not found");
+            }
+            if (array[middle] == value)
+            {
+                return middle;
+            }
+            if (value < array[middle])
+            {
+                return Search(array, value, low, middle - 1, ref numberOfComparisons);
+            }
+            
+            return Search(array, value, middle + 1, high, ref numberOfComparisons);      
+        }
+    }
+}
---
+++ Lab 10 Binary search/Form1.cs
@@ -0,0 +1,72 @@
+﻿namespace Lab_10
+{
+    public partial class Form1 : Form
+    {
+        private int[] numbers;
+
+        public Form1()
+        {
+            numbers = Array.Empty<int>();
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int position;
+            int numberOfComparisons;
+            try
+            {
+                (position, numberOfComparisons) = BinarySearch.Search(numbers, 5);
+            }
+            catch (ArgumentException exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+            List<int> positions = new List<int>() { position };
+
+            int forwardPosition = position + 1;
+            while (forwardPosition < numbers.Length && numbers[forwardPosition] == 5)
+            {
+                positions.Add(forwardPosition);
+                forwardPosition++;
+            }
+
+            int backwardPosition = position - 1;
+            while (backwardPosition >= 0 && numbers[backwardPosition] == 5)
+            {
+                positions.Add(backwardPosition);
+                backwardPosition--;
+            }
+
+            richTextBox3.AppendText($"Number of comparisons: {numberOfComparisons}\n");
+            richTextBox3.AppendText($"Positions: ");
+            foreach (var pos in positions)
+            {
+                richTextBox3.AppendText(pos.ToString() + " ");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+
+            int maxValue = Convert.ToInt32(textBox2.Text);
+            if (int.TryParse(textBox1.Text, out int numberOfElements))
+            {
+                numbers = SortedArrayGenerator.Generage(numberOfElements, maxValue);
+                richTextBox2.AppendText("Initial array:\n");
+                foreach (var item in numbers)
+                {
+                    richTextBox2.AppendText(item.ToString() + " ");
+                }
+                richTextBox2.AppendText("\n");
+            }
+            else
+            {
+                MessageBox.Show("Cannot convert to integer");
+            }
+        }
+    }
+}
